e9bb95897ead9699cc0b0e439bff6e73faeff21b
Jon <942764111@qq.com>
2017-06-23 18:31:52 +0800

    在 GameNode.js 中添加格式化时间的方法      1、  X.FormatDate     2、 Date.prototype.Format

--- src/GameNode/GameNode.js
+++ src/GameNode/GameNode.js
@@ -135,4 +135,30 @@ var X = {};
             callback & callback();
         }
     }
+
+    X.FormatDate = function(fmt){
+        var date;
+        date=fmt.substring(0,4)+'年';
+
+        date+=fmt.substring(4,6)+'月';
+
+        date+=fmt.substring(6,8)+'日';
+        return date;
+    }
 })();
+
+Date.prototype.Format = function (fmt) { //author: meizz
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
